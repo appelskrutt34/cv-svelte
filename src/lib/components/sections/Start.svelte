34b3141006ff3c401446a8295438858f906d87e7
appelskrutt34 <ebba1430@hotmail.com>
2023-05-02 15:38:12 +0200

    css

--- src/lib/components/sections/Start.svelte
+++ src/lib/components/sections/Start.svelte
@@ -7,13 +7,9 @@
 
   onMount(() => {
     draw = true;
-    setTimeout(() => {
-      image = true;
-    }, 1000);
   });
 
   let draw = false;
-  let image = false;
 </script>
 
 {#if draw}
@@ -22,12 +18,7 @@
     aria-label="Home"
     class="flex flex-col md:flex-row gap-6 justify-center items-center w-full"
   >
-    <img
-      in:fade={{ duration: 900 }}
-      class="rounded-sm max-h-96 sm:mx-10"
-      src="./99.png"
-      alt="Avatar"
-    />
+    <img class="rounded-sm max-h-96 sm:mx-10" src="./99.png" alt="Avatar" />
     <div class="w-full mt-2 sm:mt-0">
       <h1>Name: Ebba Svensson</h1>
       <h2 class="mb-1">Role: Web developer</h2>
